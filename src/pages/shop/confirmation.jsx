6136d58a715c1d49cc583d1eeb21707e7e977ee9
ymckay <yukimite@gmail.com>
2026-05-22 18:25:46 +1000

    update

--- src/pages/shop/confirmation.jsx
+++ src/pages/shop/confirmation.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 function ConfirmationPage() {
   return (
     <div className='text-center max-w-2xl mx-auto py-20'>
-      <h1 className='py-10'>Thank you</h1>
+      <h1 className='py-10 text-4xl'>Thank you</h1>
       <p className='mt-6'>Your order has been placed successfully</p>
-<Link href="/shop" className='w-fit mt-10 py-3 px-6 inline-block border'>
+<Link href="/shop" className='w-fit mt-10 py-4 px-8 uppercase tracking-[0.2em] transition  inline-block border hover:bg-accent hover:text-primary'>
 Back to Shop
 </Link>
 
