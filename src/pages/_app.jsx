3c124e265caf1e49e24a946119ef8ccdf5884c75
ymckay <yukimite@gmail.com>
2026-05-26 16:11:21 +1000

    add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,33 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/Layout";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      {/* anything in here is injected into the <head> of each page */}
-      {/* <link rel="stylesheet" href="" /> */}
-    </Head>
+      <Head>
+        <meta
+          name="description"
+          content="stem. is a modern online flower shop offering elegant bouquets and floral arrangements for everyday moments."
+        />
+        <meta name="keywords" content="flowers, florist, bouquet, floral design, online flower shop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <title>Stem. | To Make Every Moment Bloom</title>
+      </Head>
       <Layout>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-V8NSXRNSEN" strategy="afterInteractive" />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-V8NSXRNSEN');
+  `}
+        </Script>
         <Component {...pageProps} />
       </Layout>
     </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,19 +16,6 @@ function HomePage() {
     // TEMPLATE SECTION (JSX)
 
     <>
-      <Head>
-        <meta
-          name="description"
-          content="stem. is a modern online flower shop offering elegant bouquets and floral arrangements for everyday moments."
-        />
-        <meta
-          name="keywords"
-          content="flowers, florist, bouquet, floral design, online flower shop"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="UTF-8" />
-        <title>Stem. | To Make Every Moment Bloom</title>
-      </Head>
       <main>
         <HomeHero />
         <SeasonalCollection />
